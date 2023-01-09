731ae483d325318c4d133b6f03fcfd63097d5d0b
DinosMpo <dinos7591@yahoo.gr>
2023-01-09 19:35:32 +0200

    create the third section of the project

---
+++ src/component/ThirdSection/ThirdSection.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './ThirdSection.css';
+
+export default function ThirdSection() {
+	return (
+		<section id='third-section'>
+			<h1 id='third-section-title'>Access Clipboard Anywhere</h1>
+
+			<div id='third-section-description'>
+		        Whether you’re on the go, or at your computer, you can access all your Clipboard 
+		        snippets in a few simple clicks.
+			</div>
+
+			<div id='third-section-image'>
+				<img src='images/image-devices.png' alt='electronic devices' />
+			</div>
+		</section>
+	)
+}
