2ca218d939a24a315cff6612d08b4073f8226839
DinosMpo <dinos7591@yahoo.gr>
2023-01-09 19:36:49 +0200

    create the downloads buttons components of the project

---
+++ src/component/DownloadIosMac/DownloadIosMac.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './DownloadIosMac.css';
+
+export default function DownloadIosMac() {
+	return (
+		<div id='download-container'>
+	        <div id='download-ios'>Download for iOS</div>
+	        <div id='download-mac'>Download for Mac</div>
+        </div>
+	)
+}
