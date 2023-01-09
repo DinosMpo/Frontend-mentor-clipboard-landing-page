5d050a6dbe42d2fa8c9631a212e85067b2676d83
DinosMpo <dinos7591@yahoo.gr>
2023-01-09 19:36:26 +0200

    create the companies section of the project

---
+++ src/component/Companies/Companies.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Companies.css';
+
+export default function Companies() {
+	return (
+		<section id='companies-section'>
+			<div>
+				<img src="images/logo-google.png" alt="logo google" />
+			</div>
+			<div>
+				<img src="images/logo-ibm.png" alt="logo ibm" />
+			</div>
+			<div>
+				<img src="images/logo-microsoft.png" alt="logo microsoft" />
+			</div>
+			<div>
+				<img src="images/logo-hp.png" alt="logo hp" />
+			</div>
+			<div>
+				<img src="images/logo-vector-graphics.png" alt="logo vector graphics" />
+			</div>
+		</section>
+	)
+}
